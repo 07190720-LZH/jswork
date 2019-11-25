bbe4e8cfbb16278a8646860959d69f95ea4a253a
07190720-LZH <1350192224@qq.com>
2019-11-25 19:29:07 +0800

    work 12

---
+++ work12/index.js
@@ -0,0 +1,21 @@
+//获取当前年份
+let year=new Date().getFullYear()
+//调用calendar函数，设置cal内容显示年历
+document.getElementById("cal").innerHTML=calendar(year)
+
+function serCalendar(year){
+   
+   
+    //设置curyear
+   
+    //设置preyear
+   
+    //设置nextyear
+    
+    
+   
+    
+    
+   
+    //设置cal显示年历
+}
